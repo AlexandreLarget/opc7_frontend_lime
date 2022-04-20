d763e307bfbfeb27c9e30e1a55950e185d6bbec0
alex <alexandre.larget@gmail.com>
2022-04-20 13:25:13 +0800

    new version

--- dashboard.py
+++ dashboard.py
@@ -7,78 +7,106 @@ import matplotlib.pyplot as plt
 
 @st.cache
 def get_data(filename):
-    dataframe = pd.read_csv(filename, index_col=0)
-    liste_id = dataframe.SK_ID_CURR.tolist()
-    dataframe.drop("TARGET", axis=1)
-    return dataframe, liste_id
+    df = pd.read_csv(filename, index_col=0)
+    liste_id = df.SK_ID_CURR.tolist()
+    return liste_id
 
 
 @st.cache
-def get_score_explainer(filename):
-    dataframe = pd.read_csv(filename)
-    return dataframe
+def get_dataframe(filename):
+    df = pd.read_csv(filename, index_col=0)
+    return df
+
 
 
 def main():
     st.title("Outil de prédiction de crédit")
 
-    data, liste_id = get_data('data/data_scaled_sample.csv')
+    liste_id = get_data('data/liste_id.csv')
 
     st.sidebar.image('data/logo.PNG')
 
     input_id = st.sidebar.selectbox("Sélectionnez le numéro de client souhaité", liste_id)
 
-    id_data = data[data['SK_ID_CURR'] == int(input_id)].drop("SK_ID_CURR", axis=1)
-    id_data = id_data.to_dict(orient='records')[0]
-    input_dict = {}
-    input_dict['id_client'] = input_id
-
     # probabilité
     predict_btn = st.sidebar.button('Prédire')
     if predict_btn:
-        response1 = requests.post('https://opc7-backend-lime.herokuapp.com/predict', data=json.dumps(id_data, allow_nan=True))
+        response1 = requests.get('https://opc7-backend-lime.herokuapp.com/predict/' + str(input_id))
         prediction = json.loads(response1.content)
+
+        st.sidebar.write("Le modèle retourne une probabilité de solvabilité de :",str(round(prediction['1']['0'],2)),"%")
+
 
         if prediction['1']['0'] > 0.5:
             st.sidebar.success('Crédit accordé(threshold 0.5)')
         else:
-            st.sidebar.error('Crédit Refusé (threshold 0.5')
+            st.sidebar.error('Crédit Refusé (threshold 0.5)')
 
         if prediction['1']['0'] >= 0.4:
             st.sidebar.success('Crédit accordé(threshold 0.4)')
         else:
             st.sidebar.error('Crédit Refusé (threshold 0.4)')
 
-        # Explication
-        response_2 = requests.get("https://opc7-backend-lime.herokuapp.com/gal_exp/" + str(input_id))
+        # Graphique
+        response_2 = requests.get("https://opc7-backend-lime.herokuapp.com/graph/" + str(input_id))
         explanation = json.loads(response_2.content)
 
         explanation = pd.DataFrame(explanation)
-        explanation.rename(columns={'0': 'features', '1': 'value'}, inplace=True)
-        explanation['value'] = explanation['value'].astype(float)
-        explanation['features'] = explanation['features'].str.strip()
 
         fig, ax = plt.subplots()
-        ax.barh(explanation['features'], explanation['value'], color=['red' if coef < 0 else 'green' for coef in explanation['value']])
+        ax.barh(explanation['ticks'], explanation['valeur'], color=['red' if coef < 0 else 'green' for coef in explanation['valeur']])
         ax.grid(visible=True, axis='y', alpha=0.3)
         plt.xticks(rotation=45)
         st.pyplot(fig)
 
-        score_explainer = get_score_explainer("data/explain.csv")
+        # Comparaison
+
+        liste_col = explanation['ticks'].str.split(' ', expand=True)[0].values.tolist()
+
+        response_3 = requests.get("https://opc7-backend-lime.herokuapp.com/stats/"+ str(input_id))
+        stats_client = json.loads(response_3.content)
+
+        stats_client = pd.DataFrame(stats_client)
+        stats_client = stats_client[liste_col]
+        stats_client = stats_client.T
+
+        # data sur les groupes
+        stat_categ = get_dataframe('data/stats.csv')
+        stat_categ = stat_categ.T
+        stat_categ = stat_categ[liste_col]
+        stat_categ = stat_categ.T
+
+        stat_final = pd.concat([stat_categ, stats_client], axis=1)
+        new_colonnes = ['moy gpe 1', 'moy gpe 0', 'client']
+        stat_final.set_axis(new_colonnes, axis=1, inplace=True)
 
 
-        exp_detail = explanation.merge(score_explainer, left_on='features', right_on='Row', how='left')
-        exp_detail = exp_detail.loc[:,['features', 'value', 'Description']]
-        exp_detail = exp_detail.drop_duplicates('features')
-        exp_detail = exp_detail.sort_values(by='value', ascending=False, key=abs)
-        exp_detail = exp_detail.dropna(axis=0)
+        with st.expander("Comparaison par rapport aux groupes ayant et n'ayant pas eu de crédit"):
 
-        with st.expander("Explications détaillées"):
-            for i in range(exp_detail.shape[0]):
-                st.write(exp_detail.iloc[i,0])
-                st.write("Définition : {}".format(exp_detail.iloc[i, 2]))
-                st.write("------------------------------------------------------")
-                st.write(" ")
+            col1, col2 = st.columns(2)
+
+            with col1:
+                for i in [0,1,2,3,4]:
+                    fig, ax = plt.subplots(figsize=(4,2))
+                    one_row = stat_final.iloc[i, :]
+                    ax.barh([1, 2, 3], one_row.values, color=['lightseagreen', 'mediumturquoise', 'teal'])
+                    plt.yticks([1, 2, 3], one_row.index)
+                    plt.title(one_row.name)
+                    plt.xticks(rotation=45)
+                    st.pyplot(fig)
+
+            with col2:
+                for i in [5,6,7,8,9]:
+                    fig, ax = plt.subplots(figsize=(4,2))
+                    one_row = stat_final.iloc[i, :]
+                    ax.barh([1, 2, 3], one_row.values, color=['lightseagreen', 'mediumturquoise', 'teal'])
+                    plt.yticks([1, 2, 3], one_row.index)
+                    plt.title(one_row.name)
+                    plt.xticks(rotation=45)
+                    st.pyplot(fig)
+
+
+
 
 
 if __name__ == '__main__':
